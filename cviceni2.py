2c109de0c6fc69bc3847f491d90f48298f591bd9
YaroslavRyzhko <158181698+YaroslavRyzhko@users.noreply.github.com>
2025-11-01 15:31:29 +0000

    cviceni2

--- cviceni2.py
+++ cviceni2.py
@@ -1,11 +1,29 @@
-def jmeno_funkce (par1, par2):
-    return par1 + par2
+def my_range (start, stop, step = 1):
+    vys = []
+    i = start
+    while (step > 0 and i < stop):
+        vys.append(i)
+        i += step
+    return vys 
 
+def my_enumerate (seznam, start = 0):
+    vys = []
+    i = 0 
+    while i < len(seznam):
+        vys.append((start + i, seznam[i]))
+        i+=1
+    return vys
 
+if __name__ == "__main__":
+    #print(list(range(1,11)))
+    #print(list(range(1,11,2)))
 
+    #print("My range")
+    #print(my_range(1,10))
+    #print(my_range(1,10, 2))
 
-if __name__ == "main":
-    a = 2
-    b = 6
-    vys = jmeno_funkce(a, b)
-    print(vys)
+    print(list(enumerate(["a", "b", "c"])))
+    print(list(enumerate(["a", "b", "c"], 1)))
+
+    print(list(my_enumerate(["a", "b", "c"])))
+    print(list(my_enumerate(["a", "b", "c"], 1)))
