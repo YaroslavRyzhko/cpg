76008b803e7056cfce957208c5c1625aed6066e0
Ryzhko Yaroslav <ryzhko.yroslav@gmail.com>
2025-11-29 08:23:51 +0200

    úkol šest

---
+++ sixth.py
@@ -0,0 +1,33 @@
+import sys
+import requests
+from lxml import html
+
+
+def download_url_and_get_all_hrefs(url):
+    """
+    Funkce stahne url predanou v parametru url pomoci volani response = requests.get(),
+    zkontroluje navratovy kod response.status_code, ktery musi byt 200,
+    pokud ano, najdete ve stazenem obsahu stranky response.content vsechny vyskyty
+    <a href="url">odkaz</a> a z nich nactete url, ktere vratite jako seznam pomoci return
+    """
+
+    response = requests.get(url)
+    if response.status_code != 200:
+        return []
+    
+    root = html.fromstring(response.content)
+    hrefs = []
+    for href in root.xpath('//a/@href'):
+        if href.startswith('http'):
+            hrefs.append(href)
+
+    return hrefs
+
+
+if __name__ == "__main__":
+    try:
+        url = sys.argv[1]
+        print(download_url_and_get_all_hrefs(url))
+    # osetrete potencialni chyby pomoci vetve except
+    except Exception as e:
+        print(f"Program skoncil chybou: {e}")
