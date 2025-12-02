d83d29cfa2855c6fa805d30243d2a002ef4b3bd1
Ryzhko Yaroslav <ryzhko.yroslav@gmail.com>
2025-12-02 18:56:22 +0200

    úkol osm

---
+++ eighth.py
@@ -0,0 +1,23 @@
+def bin_to_dec(binarni_cislo):
+    # funkce spocita hodnotu predavaneho binarniho cisla (binarni_cislo muze byt str i int!!!)
+    # 111 -> 7
+    # "101" -> 5
+    binarni_cislo_str = str(binarni_cislo)
+    decimalna_hodnota = 0
+    mocnina = 0
+    for cislo in binarni_cislo_str:
+        decimalna_hodnota += int(cislo) *(2 ** (len(binarni_cislo_str) - mocnina - 1))
+        mocnina += 1
+    if binarni_cislo_str == "0":
+        return 0
+    else:
+        return decimalna_hodnota
+
+
+def test_bin_to_dec():
+    assert bin_to_dec("0") == 0
+    assert bin_to_dec(1) == 1
+    assert bin_to_dec("100") == 4
+    assert bin_to_dec(101) == 5
+    assert bin_to_dec("010101") == 21
+    assert bin_to_dec(10000000) == 128
